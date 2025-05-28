2f332cc1495682e3b3fbafd64060b921c6dee7e0
unknown <rishi2004vishu@gmail.com>
2025-05-28 18:58:20 +0530

    main

--- main.py
+++ main.py
@@ -1,25 +1,27 @@
 import flask
 from flask import Flask, request, jsonify
 from summarise_feed import give_text
+from pipeline import execute
 
 app= flask.Flask(__name__)
 
 @app.route('/generate-minecraft', methods=['POST'])
-def generate_minefraft():
-    from pipeline import execute
+def generate_minecraft():
     try:
         data = request.json
-        text = data.get('text',give_text())
+        print("Received data:", data)
+        text = data.get('text')
+        print("Text to be processed:", text)
         voice_id = data.get('voice_id', 'JBFqnCBsd6RMkjVDRZzb')
-        font_path = data.get('font_path', 'fonts\font.ttf')
+        font_path = data.get('font_path', 'fonts/font.ttf')
         title = data.get('title', 'You won\'t believe what just happened!')
         description = data.get('description', 'Stay updated with the latest news in just 30 seconds!')
 
-        execute(text=text, voice_id=voice_id, font_path=font_path, title=title, description=description)
+        execute(text, voice_id, font_path, title, description)
         return jsonify({"status": "success", "message": "Video generated successfully!"}), 200
 
     except Exception as e:
         return jsonify({"status": "error", "message": str(e)}), 500
     
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=5000)
